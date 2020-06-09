a40a6ad15b8b0d548286bfbfa90e1c3895144165
sanyokmalyshev <63872907+sanyokmalyshev@users.noreply.github.com>
2020-06-09 22:38:43 +0300

    additional fix

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,28 +1,47 @@
 import { Observable, fromEvent } from 'rxjs';
-import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, map, distinctUntilChanged, take, takeUntil } from 'rxjs/operators';
 
-const search$ = new Observable<Event>(observer => {
-    const search = document.getElementById('search');
+// const search$ = new Observable<Event>(observer => {
+//     const search = document.getElementById('search');
+//     const stop = document.getElementById('stop');
 
-    if (!search) {
-        observer.error('Element not exists on the page');
-        return
-    }
+//     if (!search || !stop) {
+//         observer.error('Element not exists on the page');
+//         return
+//     }
 
-    search.addEventListener('input', ev => {
-        console.log(123);
-        observer.next(ev);
-        observer.complete();
+//     const onSearch = ev => {
+//         console.log(123);
+//         checkSubscription();
+//         observer.next(ev);
 
-    });
+//     }
 
-});
+//     const onStop = ev => {
+//         checkSubscription();
+//         observer.complete();
+//         clear();
+//     };
 
-// const search$: Observable<Event> = fromEvent<Event>(
-//     document.getElementById('search'), 'input');
+//     search.addEventListener('input', onSearch);
+//     stop.addEventListener('click', onStop)
 
+//     const checkSubscription = () => {
+//         if(observer.closed) {
+//             clear();
+//         }
+//     }
 
+//     const clear = () => {
+//         search.removeEventListener('input', onSearch);
+//     };
+// });
 
+const search$: Observable<Event> = fromEvent<Event>(
+    document.getElementById('search'), 'input');
+
+const stop$: Observable<Event> = fromEvent<Event>(
+    document.getElementById('stop'), 'click');
 
 search$.pipe(
     map(ev => {
@@ -30,7 +49,8 @@ search$.pipe(
     }),
     debounceTime(500),
     map(value => value.length > 3 ? value : ''),
-    distinctUntilChanged()
+    distinctUntilChanged(),
+    takeUntil(stop$)
 )
     .subscribe(value => {
         console.log(value);
